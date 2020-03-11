1b0906ba9cffac1df98a0e3b6be7df51e116f9f9
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-11 22:11:25 +0000

    Create class Github & add getUser

---
+++ github.js
@@ -0,0 +1,16 @@
+class Github {
+  constructor() {
+    this.client_id = 'e15fff46bdc29888bebf';
+    this.client_secret = 'cb2d25f62aea14773e8f9b9bb50af7ecbc2e79d0';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_sicret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile: profile
+    }
+  }
+}
