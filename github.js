a262ada902c764a083f26521c04d0f0c180aa559
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-12 13:00:04 +0000

    output repos

--- github.js
+++ github.js
@@ -2,7 +2,7 @@ class Github {
   constructor() {
     this.client_id = 'e15fff46bdc29888bebf';
     this.client_secret = 'cb2d25f62aea14773e8f9b9bb50af7ecbc2e79d0';
-    this.repos_count = 6;
+    this.repos_count = 5;
     this.repos_score = 'created: asc';
   }
 
@@ -12,7 +12,7 @@ class Github {
     const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
-    const repos = await reposResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
